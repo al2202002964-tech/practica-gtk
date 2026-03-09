647bbbf96eda6413e1d764fc30045bae7063431f
al2202002964-tech <al2202002964@azc.uam.mx>
2026-03-08 23:38:42 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,37 @@
 #include <gtk/gtk.h>
 
+/* funcion que se ejecuta cuando se presiona el boton */
+static void boton_presionado(GtkWidget *widget, gpointer data)
+{
+    GtkWidget *dialog;
+
+    dialog = gtk_message_dialog_new(NULL,
+                                    GTK_DIALOG_MODAL,
+                                    GTK_MESSAGE_INFO,
+                                    GTK_BUTTONS_OK,
+                                    "Hola! Presionaste el boton");
+
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+}
+
+/* funcion principal que crea la ventana */
 static void activate(GtkApplication *app, gpointer user_data)
 {
     GtkWidget *window;
+    GtkWidget *button;
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Practica GTK");
     gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
 
-    gtk_widget_show(window);
+    button = gtk_button_new_with_label("Presioname");
+
+    g_signal_connect(button, "clicked", G_CALLBACK(boton_presionado), NULL);
+
+    gtk_container_add(GTK_CONTAINER(window), button);
+
+    gtk_widget_show_all(window);
 }
 
 int main(int argc, char **argv)
@@ -17,9 +40,12 @@ int main(int argc, char **argv)
     int status;
 
     app = gtk_application_new("org.practica.gtk", G_APPLICATION_FLAGS_NONE);
+
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+
     status = g_application_run(G_APPLICATION(app), argc, argv);
 
     g_object_unref(app);
+
     return status;
 }
